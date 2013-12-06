c802357c47abe9d8b263eb9bba2364c5c94e0e52
JeffreyWang <quanwang06@gmail.com>
2013-12-07 00:02:55 +0800

    fix two head bug

--- protected/views/user/create.php
+++ protected/views/user/create.php
@@ -2,11 +2,6 @@
 /* @var $this UserController */
 /* @var $model User */
 ?>
-    <div class="header">
-        <div class="com-cent">
-            <div class="hd-title">微信 房产管理后台</div>
-        </div>
-    </div>
     <h1>新增用户</h1>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
