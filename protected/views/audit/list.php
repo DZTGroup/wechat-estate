20e440f7e5de4db80b91ac287405f71906a27195
JeffreyWang <quanwang06@gmail.com>
2013-12-08 20:48:08 +0800

    add list all passed data

--- protected/views/audit/list.php
+++ protected/views/audit/list.php
@@ -1,12 +1,20 @@
+<script>
+    $(document).ready(function(){
+        WXAPP.AuditAdmin.listAllPassedData();
+    });
+</script>
+
 <div class="cent-auto">
     <!-- 管理列表表单【【 -->
     <div class="box-table"><!-- 添加hide隐藏 -->
-        <table width="760" border="0" cellspacing="1" cellpadding="0" bgcolor="#d7d7d7" id="J_audit_table">
+        <table width="760" border="0" cellspacing="1" cellpadding="0" bgcolor="#d7d7d7" id="J_audit_list_all_passed_data_table">
             <thead>
             <tr>
                 <th>楼盘ID</th>
                 <th>楼盘名称</th>
+                <th>审核类型</th>
                 <th>提交时间</th>
+                <th>状态</th>
                 <th>操作</th>
             </tr>
             </thead>
@@ -14,5 +22,4 @@
             </tbody>
         </table>
     </div>
-    <!-- 管理列表表单 】】 -->
 </div>
