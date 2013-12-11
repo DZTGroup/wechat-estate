b0cd8e400a6ccec21d7146d196c7ca510958052e
adam mao <saalih@qq.com>
2013-12-11 23:14:24 +0800

    test

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,7 +2,7 @@
 /*
  * Database Configuration
  */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '112.124.55.78');
 define('DB_USER', 'zunhao');
 define('DB_PASS', '655075d7dd');
 define('DB_DATABASENAME', 'wxfc');
