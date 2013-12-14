0f8c20ad3742d393445461e53b751dcca968478a
mangix <maqh1988@gmail.com>
2013-12-14 21:46:14 +0800

    heh

--- js/base.js
+++ js/base.js
@@ -235,6 +235,9 @@ window.WXAPP = window.WXAPP || {};
     var form = $('#J_entity_form'),
         newBtn = $('#J_entity_new'),
         table = $('#J_entity_table tbody');
+    if (!form.length) {
+        return;
+    }
     var entity = new WXAPP.Entity(form.attr('data-type'), form, table, newBtn, {
         multiple: form.attr('data-multiple') === "true"
     });
@@ -260,40 +263,45 @@ window.WXAPP = window.WXAPP || {};
     });
 
     //图片上传
-    $('.J_upload').each(function (i, button) {
-        var id = 'J_upload' + (+new Date());
-        $(button).attr('id', id);
-        var infoSpan = $(button).parent().parent().find('.J_display');
-        var swfu = new SWFUpload({
-            upload_url: "upload_file.php",
-            flash_url: "js/upload/flash/swfupload.swf",
-            flash9_url: "http://www.swfupload.org/swfupload_fp9.swf",
-            file_size_limit: "100 KB", //文件大小限制
-            button_placeholder_id: id,
-            button_width: 83,
-            button_height: 31,
-            button_image_url: 'img/upload.png',
-            file_post_name: 'file',
-            file_dialog_complete_handler: function () {
-                this.addPostParam('estate_id', window.getEstateId());
-                this.startUpload();
+    WXAPP.bindLoad = function (btns) {
+        btns.each(function (i, button) {
+            var id = 'J_upload' + (+new Date());
+            $(button).attr('id', id);
+            var infoSpan = $(button).parent().parent().find('.J_display');
+            var swfu = new SWFUpload({
+                upload_url: "upload_file.php",
+                flash_url: "js/upload/flash/swfupload.swf",
+                flash9_url: "http://www.swfupload.org/swfupload_fp9.swf",
+                file_size_limit: "100 KB", //文件大小限制
+                button_placeholder_id: id,
+                button_width: 83,
+                button_height: 31,
+                button_image_url: 'img/upload.png',
+                file_post_name: 'file',
+                file_dialog_complete_handler: function () {
+                    this.addPostParam('estate_id', window.getEstateId());
+                    this.startUpload();
 
-                infoSpan.find('.info').html('正在上传..');
-            },
-            upload_progress_handler: function () {
-                //console.log(arguments);
-            },
-            upload_success_handler: function (file, res) {
-                res = JSON.parse(res);
-                if (res.code == 200) {
-                    infoSpan.find('.info').html('上传成功!');
-                    infoSpan.find('img').val(res.data.id).attr('src', res.data.src);
-                } else {
-                    alert(res.data.msg);
+                    infoSpan.find('.info').html('正在上传..');
+                },
+                upload_progress_handler: function () {
+                    //console.log(arguments);
+                },
+                upload_success_handler: function (file, res) {
+                    res = JSON.parse(res);
+                    if (res.code == 200) {
+                        infoSpan.find('.info').html('上传成功!');
+                        infoSpan.find('img').val(res.data.id).attr('src', res.data.src);
+                    } else {
+                        alert(res.data.msg);
+                    }
                 }
-            }
+            });
         });
-    });
+    }
+
+    WXAPP.bindLoad($('.J_upload'))
+
     var estate_id;
     window.setEstateId = function (id) {
         estate_id = id;
@@ -589,50 +597,377 @@ window.WXAPP = window.WXAPP || {};
 })();
 
 
-(function(){
+(function () {
     function postListSuccessCallback(res) {
         var postBody = $('#topic_id');
         res.data.forEach(function (item) {
             postBody.append('<div class="mod-box" onclick="showDetail();">'
-                +'<h3 class="mod-box__title ui-mb-small">'
-                +item.title+'</h3>'
-                +'<div class="mod-box__time">'
-                +'<span class="ui-fl-l ui-c-light">'+item.wechat_id+' '+item.create_time+'</span>'
-                +'<span class="ui-c-light"><span class="icon-eye"></span>浏览 （11）</span></div>'
-                +'<div class="mod-box__content ui-mb-medium">'
-                +item.content+'</div>'
-                +'<div class="mod-box__control">'
-                +'<i class="icon-heart"></i></span>浏览 （11）</span>'
-                +'<i class="icon-comment"></i><span>评论（1）</span>'
-                +'</div>'
-                +'</div>')
+                + '<h3 class="mod-box__title ui-mb-small">'
+                + item.title + '</h3>'
+                + '<div class="mod-box__time">'
+                + '<span class="ui-fl-l ui-c-light">' + item.wechat_id + ' ' + item.create_time + '</span>'
+                + '<span class="ui-c-light"><span class="icon-eye"></span>浏览 （11）</span></div>'
+                + '<div class="mod-box__content ui-mb-medium">'
+                + item.content + '</div>'
+                + '<div class="mod-box__control">'
+                + '<i class="icon-heart"></i></span>浏览 （11）</span>'
+                + '<i class="icon-comment"></i><span>评论（1）</span>'
+                + '</div>'
+                + '</div>')
         });
     }
-    var Post={
-        getListData:function(estate_id){
+
+    var Post = {
+        getListData: function (estate_id) {
             WXAPP.Ajax('?r=post/ajaxgetpostlist', {
-                estate_id:estate_id
+                estate_id: estate_id
             }, postListSuccessCallback);
         }
 
     };
     WXAPP.Post = Post;
 
-    
-    var post_form=$('#bbs_post_form');
-    var current_estate_id=$('#estate_id').val();
-    var wechat_id=$('#wechat_id').val();
 
-    $('#post_btnSend').click(function(){
-        var title=post_form.find('#tfTitle').val();
-        var content=post_form.find('#tfContent').val();
+    var post_form = $('#bbs_post_form');
+    var current_estate_id = $('#estate_id').val();
+    var wechat_id = $('#wechat_id').val();
+
+    $('#post_btnSend').click(function () {
+        var title = post_form.find('#tfTitle').val();
+        var content = post_form.find('#tfContent').val();
 
         WXAPP.Ajax('?r=post/ajaxpostnewpost', {
-            estate_id: current_estate_id,wechat_id:wechat_id,post_title:title,post_content:content
-        }, function(res){
-            if(res.code==200){
-               location.href='?r=post/list&estate_id='+current_estate_id;
+            estate_id: current_estate_id, wechat_id: wechat_id, post_title: title, post_content: content
+        }, function (res) {
+            if (res.code == 200) {
+                location.href = '?r=post/list&estate_id=' + current_estate_id;
             }
         });
     });
+})();
+
+
+//户型管理
+(function () {
+    var form = $('#J_apartment_form'),
+        newBtn = $('#J_entity_new'),
+        table = $('#J_entity_table tbody');
+    if (!form.length) {
+        return;
+    }
+    var entity = new WXAPP.Entity(form.attr('data-type'), form, table, newBtn, {
+        multiple: form.attr('data-multiple') === "true"
+    });
+
+    form.find('.J_add').click(function(){
+        entity.empty();
+    });
+
+    var holder = $('.J_holder');
+
+    var html = '<div class="J_loop">' +
+        '<div class="com-min">' +
+        '<h3>户型分类：<input class="J_type_name" name="type_name" ></h3>' +
+        '</div><ul class="J_type_list"></ul></div>';
+    var typeList1 = '<li>户型名：<span class="J_name"></span><input type="hidden"><button class="btn-cha J_edit" type="button">编辑</button></li>';
+
+    entity.getData = function () {
+        var data = [];
+        holder.find('.J_loop').each(function (i, loop) {
+            var typeData = {};
+            typeData.type_name = $(loop).find('.J_type_name').val();
+            typeData.type_list = [];
+            $(loop).find('.J_type_list input[type=hidden]').each(function (i, item) {
+                typeData.type_list.push(JSON.parse($(item).val()));
+            });
+            data.push(typeData);
+        });
+
+        return data;
+
+    }
+    function edit(data, type, cb) {
+        var layer = $('<div class="box-layer" style="width:700px;">' +
+            ' <div class="title"><h2>户型数据</h2></div>' +
+            '<a class="close J_cancel" href="javascript:;" title="关闭">关闭</a>' +
+            '<div class="tip-mian">' +
+            '<div class="p-shuju">' +
+            '<div class="layer-lb">' +
+            '<label>户型名：</label>' +
+            '<input class="inp-tex inp-300 J_field" name="name" type="text">' +
+            '</div> ' +
+            '<div class="layer-lb">' +
+            '<label>户型分类：</label>' +
+            '<input class="inp-tex inp-300 J_type" name="type" type="text">' +
+            '</div>' +
+            '<div class="layer-lb">' +
+            '<label>户型简介：</label>' +
+            '<input class="inp-tex inp-300 J_field" name="desc" type="text">' +
+            '</div>' +
+            '<div class="layer-lb">' +
+            '<label>建筑面积：</label>' +
+            '<input class="inp-tex inp-300 J_field" name="area" type="text">' +
+            '</div>' +
+            '<div class="layer-lb">' +
+            '<label>套内面积：</label>' +
+            '<input class="inp-tex inp-300 J_field" name="inner_area" type="text">' +
+            '</div>' +
+            '<div class="layer-lb">' +
+            '<label>楼层：</label>' +
+            '<input class="inp-tex inp-300 J_field" name="floors" type="text">' +
+            '</div>' +
+            '<div class="layer-lb">' +
+            '<label>详细信息：</label>' +
+            '<textarea class="layer-kuang J_field" name="detail" cols="" rows="" placeholder="100个字以内"></textarea>' +
+            '</div>' +
+            '</div>' +
+            '<div class="lay-sctp">' +
+            '<p><button class="btn-cha J_edit_type" type="button">编辑户型图</button></p>' +
+            '<p><button class="btn-cha J_edit_all" type="button">编辑全景图</button></p>' +
+            '(推荐图片尺寸：720*130；图片小于100k)' +
+            '</div>' +
+            '<div class="but-auto">' +
+            '<a class="an-butn J_sure" href="javascript:;" title="确认">确认</a> ' +
+            '<a class="an-butn J_cancel" href="javascript:;" title="取消">取消</a>' +
+            '</div></div></div>').appendTo('body');
+
+        if (data) {
+            $('.J_field').each(function (i, item) {
+                $(item).val(data['base-info'][$(item).attr('name')]);
+            });
+            $('.J_type').val(type);
+        } else {
+            data = {
+                'base-info': {},
+                'panoramagram': [],
+                'home-plan': []
+            };
+            type = "";
+        }
+
+        layer.find('.J_sure').click(function () {
+            var type = $('.J_type').val();
+            data['base-info'] = {};
+            layer.find('.J_field').each(function (i, field) {
+                data['base-info'][$(field).attr('name')] = $(field).val();
+            });
+            if (cb) {
+                cb(data, type);
+            }
+            alert('编辑成功，不要忘记提交');
+            layer.remove();
+        });
+        layer.find('.J_edit_all').click(function () {
+            if (data) {
+                showEditPanoramagram(data.panoramagram, function (newPa) {
+                    data.panoramagram = newPa;
+                });
+            }
+        });
+        layer.find('.J_edit_type').click(function () {
+            if (data) {
+                showEditType(data['home-plan'], function (newPa) {
+                    data['home-plan'] = newPa;
+                });
+            }
+        });
+        layer.find('.J_cancel').click(function () {
+            layer.remove();
+        })
+    }
+
+    function showEditType(data, cb) {
+        var layer = $('<div class="box-layer" style="width:700px;">' +
+            ' <div class="title"><h2>户型图</h2></div>' +
+            ' <a class="close J_cancel" href="javascript:;" title="关闭">关闭</a>' +
+            ' <div class="tip-mian">' +
+            ' <div class="p-shuju">' +
+            ' </div>' +
+            ' <div class="but-auto"><a class="an-butn J_add" href="javascript:;" title="添加">添加户型图</a> <a class="an-butn J_sure" href="javascript:;" title="确定">确定</a></div>' +
+            ' </div> </div>').appendTo('body');
+        var html = '<div class="J_item"><input class="J_field" name="name" >' +
+            '<div><span class="load_btn"> <span class="btn-cha J_upload"></span></span>' +
+            '<div class="J_display">' +
+            '<img src="" class="J_field" name="img" value="">' +
+            '</div></div>'
+        '</div>';
+
+        if (data) {
+            data.forEach(function (item) {
+                var list = $(html).appendTo(layer.find('.p-shuju'));
+                list.find('.J_field').each(function (j, field) {
+                    $(field).val(item[$(field).attr('name')]);
+                    if (field.nodeName.toLowerCase() === "img") {
+                        field.src = 'upload_files/' + entity.estate_id + "/" + item[$(field).attr('name')];
+                    }
+                });
+
+            });
+        }
+        WXAPP.bindLoad(layer.find('.J_upload'));
+
+        layer.find('.J_add').click(function () {
+            var list = $(html).appendTo(layer.find('.p-shuju'));
+            WXAPP.bindLoad(list.find('.J_upload'));
+        });
+
+        layer.find('.J_sure').click(function () {
+            var list = [];
+            layer.find('.J_item').each(function (i, item) {
+                var data = {};
+                $(item).find('.J_field').each(function (i, field) {
+                    data[$(field).attr('name')] = $(field).val();
+                });
+                list.push(data);
+            });
+            alert('户型图修改成功');
+            layer.remove();
+            cb(list);
+        });
+        layer.find('.J_cancel').click(function () {
+            layer.remove();
+        })
+
+    }
+
+    function showEditPanoramagram(data, cb) {
+        var layer = '<div class="box-layer" style="width:600px;">' +
+            ' <div class="title"><h2>户型数据</h2></div>' +
+            ' <a class="close J_cancel" href="javascript:;" title="关闭">关闭</a>' +
+            ' <div class="tip-mian">' +
+            ' <div class="p-shuju">' +
+            ' </div>' +
+            ' <div class="but-auto"><a class="an-butn J_add" href="javascript:;" title="添加全景">添加全景</a> <a class="an-butn J_sure" href="javascript:;" title="确定">确定</a></div>' +
+            ' </div>' +
+            ' </div>';
+        layer = $(layer).appendTo('body');
+        var html = '<div class="J_item"><div class="layer-lb"><label>全景名称：</label> <input class="inp-tex inp-300 J_field" name="name" type="text"></div>' +
+            '<div class="layer-lb"><label>访问地址：</label> <input class="inp-tex inp-300 J_field" name="link" type="text"></div></div>';
+
+        if (data) {
+            data.forEach(function (dataItem) {
+                var item = $(html).appendTo(layer.find('.p-shuju'));
+                item.find('.J_field').each(function (i, field) {
+                    $(field).val(dataItem[$(field).attr('name')]);
+                });
+            });
+        }
+        layer.find('.J_add').click(function () {
+            $(html).appendTo(layer.find('.p-shuju'));
+        });
+        layer.find('.J_sure').click(function () {
+            var Panoramagram = [];
+            layer.find('.J_item').each(function (i, item) {
+                var data = {};
+                $(item).find('.J_field').each(function (j, field) {
+                    data[$(field).attr('name')] = $(field).val();
+                });
+                Panoramagram.push(data);
+            });
+            cb(Panoramagram);
+            alert('全景图编辑成功!');
+            layer.remove();
+        });
+        layer.find('.J_cancel').click(function () {
+            layer.remove();
+        })
+
+    }
+
+    entity.setData = function (res) {
+        var data = JSON.parse(res.content);
+        var self = this;
+        holder.empty();
+        data.forEach(function (typeData) {
+            var l = $(html).appendTo(holder);
+            l.find('.J_type_name').val(typeData.type_name);
+            typeData.type_list.forEach(function (type) {
+                var list = $(typeList1).appendTo(l.find('ul'));
+                list.find('.J_name').html(type['base-info'].name);
+                list.find('input[type=hidden]').val(JSON.stringify(type));
+                list.find('.J_edit').click(function () {
+                    edit(type, typeData.type_name, function (d) {
+                        list.find('input[type=hidden]').val(JSON.stringify(d));
+                    });
+                });
+            });
+        });
+    }
+
+    entity.empty = function () {
+        edit(null, null, function (d, type) {
+            var l = null
+            holder.find(".J_loop").each(function (i, loop) {
+                if ($(loop).find('.J_type_name').val() === type) {
+                    l = $(loop);
+                }
+            });
+            if (!l) {
+                l = $(html).appendTo(holder);
+            }
+            l.find('.J_type_name').val(type);
+            var list = $(typeList1).appendTo(l.find('ul'));
+            list.find('.J_name').html(d['base-info'].name);
+            list.find('input[type=hidden]').val(JSON.stringify(d));
+            list.find('.J_edit').click(function () {
+                edit(d, type, function (d) {
+                    list.find('input[type=hidden]').val(JSON.stringify(d));
+                });
+            });
+        });
+    };
+})();
+
+//专家点评
+(function(){
+    var html='<div class="J_module" data-module="expert">' +
+        ' <div class="tipe-lb "><label>专家名字：</label> <input class="inp-tex inp-300 J_field" name="name" type="text">' +
+        ' </div>' +
+        ' <div class="tipe-lb"><label>专家头街：</label> <input class="inp-tex inp-300 J_field" name="title" type="text" placeholder="(10个字以内)">' +
+        ' </div>' +
+        ' <div class="tipe-lb"><label>专家介绍：</label> <textarea class="text-kuang J_field" name="desc" cols="" rows="" placeholder="100个字以内"></textarea>' +
+        ' </div>' +
+        ' <div class="tipe-lb"><label>点评标语：</label> <input class="inp-tex inp-300 J_field" name="c_title" type="text" placeholder="(15个字以内)">' +
+        ' </div>' +
+        ' <div class="tipe-lb"><label>点评内容：</label> <textarea class="text-kuang J_field" name="c_content" cols=""' +
+        ' rows="" placeholder="100个字以内"></textarea></div>' +
+        ' </div><hr/>'
+
+    var form = $('#J_expert_form'),
+        newBtn = $('#J_entity_new'),
+        table = $('#J_entity_table tbody');
+    if (!form.length) {
+        return;
+    }
+    form.find('.J_add').click(function(){
+        entity.empty();
+    })
+    var entity = new WXAPP.Entity(form.attr('data-type'), form, table, newBtn, {
+        multiple: form.attr('data-multiple') === "true"
+    });
+    entity.setData = function(rs){
+        if(rs){
+            var data = JSON.parse(rs.content)
+            data.forEach(function(expert){
+                var l = $(html).appendTo(form.find('.J_expert_holder'));
+                l.find('.J_field').each(function(i,field){
+                    $(field).val(expert[$(field).attr('name')]);
+                });
+            });
+        }
+    }
+    entity.getData = function(){
+        var list = [];
+        form.find('.J_module').each(function(i,module){
+            var data = {};
+            $(module).find('.J_field').each(function(i,field){
+                data[$(field).attr('name')] = $(field).val();
+            });
+            list.push(data);
+        });
+        return list;
+    }
+    entity.empty = function(){
+        $(html).appendTo(form.find('.J_expert_holder'));
+    }
 })();
