c1a9e6c2b73d4229c8e0814a4a9ca21ca54db7eb
JeffreyWang <quanwang06@gmail.com>
2014-01-04 12:47:13 +0800

    a

--- protected/views/statistic/chart.php
+++ protected/views/statistic/chart.php
@@ -18,10 +18,12 @@
                 date_default_timezone_set('Asia/Shanghai');
     ?>
 
+    var chart_old=null;
+
     $('.J_estate_list').change(function(){
         debugger
         var estate_id = $('.J_estate_list').val();
-        var chart_old=null;
+
 
         WXAPP.Ajax('?r=statistic/ajaxgetfollownum',{
             eid:estate_id
